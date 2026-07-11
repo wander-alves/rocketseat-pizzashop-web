eb1fd1592357197cbd3c6ffe378d3b651f2b7239
Wanderson Alves do Nascimento <wanderson.alves.n@outlook.com>
2026-07-11 12:47:20 -0300

    installing shadcn-ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
+import "@/styles/global.css";
+import { Button } from "@/components/ui/button"
+
 function App() {
   return (
-    <h1>Hello World!</h1>
+    <div className="flex justify-center items-center h-screen">
+      <Button>Enviar</Button>
+    </div>
   )
 }
 

3bf4123ac77de8785e7ad7298c8d28b76b2e5549
Wanderson Alves do Nascimento <wanderson.alves.n@outlook.com>
2026-07-12 00:11:05 -0300

    style: add eslint, prettier and configs

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import "@/styles/global.css";
-import { Button } from "@/components/ui/button"
+import '@/styles/global.css'
+
+import { Button } from '@/components/ui/button'
 
 function App() {
   return (
-    <div className="flex justify-center items-center h-screen">
+    <div className="flex h-screen items-center justify-center">
       <Button>Enviar</Button>
     </div>
   )

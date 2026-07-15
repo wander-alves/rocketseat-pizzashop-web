a0ee2a05871d99715b4fee9f775faf53a2e7d262
Wanderson Alves do Nascimento <wanderson.alves.n@outlook.com>
2026-07-15 14:08:56 -0300

    feat: create auth base layout and signin base page

--- src/pages/_layouts/auth.tsx
+++ src/pages/_layouts/auth.tsx
@@ -1,13 +1,24 @@
+import { Pizza } from 'lucide-react'
 import { Outlet } from 'react-router-dom'
 
 function AuthLayout() {
   return (
-    <>
-      <div>Autenticação</div>
-      <div>
+    <div className="grid min-h-screen grid-cols-2">
+      <div className="border-foreground/5 bg-muted text-muted-foreground flex h-full flex-col justify-between border-r p-10">
+        <div className="text-foreground flex items-center gap-3 text-lg">
+          <Pizza className="h-5 w-5" />
+          <span className="font-semibold">pizza.shop</span>
+        </div>
+
+        <footer className="text-sm">
+          Painel do parceiro &copy; pizza.shop - {new Date().getFullYear()}
+        </footer>
+      </div>
+
+      <div className="flex flex-col items-center justify-center">
         <Outlet />
       </div>
-    </>
+    </div>
   )
 }
 
--- src/pages/auth/signin.tsx
+++ src/pages/auth/signin.tsx
@@ -1,8 +1,35 @@
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+
 function Signin() {
   return (
     <>
       <title>Signin | pizza.shop</title>
-      <h1>Signin</h1>
+
+      <div className="p-8">
+        <div className="flex w-[350px] flex-col justify-center gap-6">
+          <div className="flex flex-col gap-2 text-center">
+            <h1 className="text-2xl font-semibold tracking-tight">
+              Acessar Painel
+            </h1>
+            <p className="text-muted-foreground text-sm">
+              Acompanhe suas vendas pelo painel do parceiro.
+            </p>
+          </div>
+
+          <form className="space-y-4">
+            <div className="space-y-2">
+              <Label htmlFor="email">Seu e-mail</Label>
+              <Input id="email" type="email" />
+            </div>
+
+            <Button className="w-full" type="submit">
+              Accessar Painel
+            </Button>
+          </form>
+        </div>
+      </div>
     </>
   )
 }

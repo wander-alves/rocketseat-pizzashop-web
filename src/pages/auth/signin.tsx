ec3db4fd2078d09094953359657990bfdb43daeb
Wanderson Alves do Nascimento <wanderson.alves.n@outlook.com>
2026-07-16 00:18:34 -0300

    feat: add react-hook-form and zod for form handle

--- src/pages/auth/signin.tsx
+++ src/pages/auth/signin.tsx
@@ -1,8 +1,29 @@
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 
+const signinDataSchema = z.object({
+  email: z.email(),
+})
+
+type SigninData = z.infer<typeof signinDataSchema>
+
 function Signin() {
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<SigninData>()
+
+  async function handleSignin(data: SigninData) {
+    await new Promise((resolve) => setTimeout(resolve, 2000))
+
+    console.log(data)
+  }
+
   return (
     <>
       <title>Signin | pizza.shop</title>
@@ -18,13 +39,13 @@ function Signin() {
             </p>
           </div>
 
-          <form className="space-y-4">
+          <form className="space-y-4" onSubmit={handleSubmit(handleSignin)}>
             <div className="space-y-2">
               <Label htmlFor="email">Seu e-mail</Label>
-              <Input id="email" type="email" />
+              <Input id="email" type="email" {...register('email')} />
             </div>
 
-            <Button className="w-full" type="submit">
+            <Button className="w-full" type="submit" disabled={isSubmitting}>
               Accessar Painel
             </Button>
           </form>

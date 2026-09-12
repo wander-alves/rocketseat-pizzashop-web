4242156234cb622bc6f8df561be79f55fe3f1f62
Wanderson Alves do Nascimento <wanderson.alves.n@outlook.com>
2026-09-12 20:07:45 -0300

    feat: add `OrderStatus` test

--- src/components/order-status.spec.tsx
+++ src/components/order-status.spec.tsx
@@ -1,5 +1,58 @@
-import { test, expect } from 'vitest';
+import { describe, afterEach, it, expect  } from 'vitest';
+import { render, type RenderResult } from '@testing-library/react';
+import { OrderStatus } from './order-status';
 
-test('Test sum', ()=> {
-  expect(1+1).toEqual(2);
-})
+
+describe('Order Status', ()=> {
+  let wrapper: RenderResult;
+  
+  afterEach(()=> {
+    wrapper.unmount();
+  });
+
+  it('should display the correct text when order status is pending', ()=> {
+    wrapper = render(<OrderStatus status='pending' />);
+    const statusText = wrapper.getByText('Pendente');
+    const badgeElement = wrapper.getByTestId('badge');
+    
+    expect(statusText).toBeInTheDocument();
+    expect(badgeElement).toHaveClass('bg-slate-400');
+    wrapper.unmount();
+  });
+  
+  it('should display the correct text when order status is processing', ()=> {
+    wrapper = render(<OrderStatus status='processing' />);
+    const statusText = wrapper.getByText('Em preparo');
+    const badgeElement = wrapper.getByTestId('badge');
+    
+    expect(statusText).toBeInTheDocument();
+    expect(badgeElement).toHaveClass('bg-amber-500');
+  });
+
+  it('should display the correct text when order status is delivering', ()=> {
+    wrapper = render(<OrderStatus status='delivering' />);
+    const statusText = wrapper.getByText('Em entrega');
+    const badgeElement = wrapper.getByTestId('badge');
+    
+    expect(statusText).toBeInTheDocument();
+    expect(badgeElement).toHaveClass('bg-amber-500');
+  });
+
+  it('should display the correct text when order status is delivered', ()=> {
+    wrapper = render(<OrderStatus status='delivered' />);
+    const statusText = wrapper.getByText('Entregue');
+    const badgeElement = wrapper.getByTestId('badge');
+    
+    expect(statusText).toBeInTheDocument();
+    expect(badgeElement).toHaveClass('bg-emerald-500');
+  });
+
+  it('should display the correct text when order status is canceled', ()=> {
+    wrapper = render(<OrderStatus status='canceled' />);
+    const statusText = wrapper.getByText('Cancelado');
+    const badgeElement = wrapper.getByTestId('badge');
+    
+    expect(statusText).toBeInTheDocument();
+    expect(badgeElement).toHaveClass('bg-rose-500');
+  });
+});

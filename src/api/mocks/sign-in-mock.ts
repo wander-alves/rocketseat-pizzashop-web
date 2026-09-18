795c3c695978854041b21869712343d2bbde6ed6
Wanderson Alves do Nascimento <wanderson.alves.n@outlook.com>
2026-09-17 23:16:23 -0300

    feat: add sigin mock

---
+++ src/api/mocks/sign-in-mock.ts
@@ -0,0 +1,19 @@
+import { http, HttpResponse } from 'msw';
+import type { SignInBody } from './sign-in';
+
+const signInMock = http.post<never, SignInBody>('/authenticate', async ({ request })=> {
+  const { email } = await request.json();
+
+  if(email === 'john.doe@example.com') {
+    return new HttpResponse(null, {
+      status: 200,
+      headers: {
+        'Set-Cookie': 'auth=sample-jwt',
+      },
+    });
+  }
+
+  return new HttpResponse(null, { status: 401 });
+}); 
+
+export { signInMock }

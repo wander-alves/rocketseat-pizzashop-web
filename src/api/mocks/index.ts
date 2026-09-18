7f9dbb8eb3c9883b930bb46bf8d2748691b0defd
Wanderson Alves do Nascimento <wanderson.alves.n@outlook.com>
2026-09-17 23:16:23 -0300

    feat: add sigin mock

--- src/api/mocks/index.ts
+++ src/api/mocks/index.ts
@@ -1,8 +1,11 @@
 import { setupWorker } from 'msw/browser';
 
 import { env } from '@/lib/env';
+import { signInMock } from '../sign-in-mock';
 
-const worker = setupWorker();
+const worker = setupWorker(
+  signInMock,
+);
 
 async function enableMSW() {
   if(env.MODE !== 'test') {
